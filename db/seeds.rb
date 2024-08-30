866a2540ddd7279303cbb2926f5364f3e189bbd4
KhoiPham <pham.tran.minh.khoi@moneyforward.vn>
2024-08-30 14:47:02 +0700

    Update seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# create 5 users
+(0..5).each do |i|
+  User.create(
+    email: "example#{i}@example.com",
+    password: "password",
+    password_confirmation: "password"
+  )
+end
+
+# create 50 seats
+(0..49).each do |i|
+  Seat.create(name: "Seat #{i}")
+end
